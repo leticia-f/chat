82d9e7c3c3064b855da6d3f793d00a82de97bb0e
JulioVianaDev <JulioVianaDev@gmail.com>
2021-11-03 13:55:27 -0300

    subindo o projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {ChatEngine} from 'react-chat-engine';
+
+const App = ()=>{
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="21933e86-b564-4e5e-a6f8-2b0f0a307544"
+            userName="Tomate"
+            userSecret="super123"
+
+        />
+    )
+}
+export default App;
