562411ae0d4d2ed06ecbe15f5d01c62834bc0f94
JulioVianaDev <JulioVianaDev@gmail.com>
2021-11-10 17:46:06 -0300

    mensagem

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import {ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 
-const App = ()=>{
-    return(
-        <ChatEngine
-            height="100vh"
-            projectID="21933e86-b564-4e5e-a6f8-2b0f0a307544"
-            userName="Tomate"
-            userSecret="super123"
+import './App.css';
 
-        />
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
+const App = () => {
+
+  return (
+    <ChatEngine
+    height="100vh"
+    projectID="21933e86-b564-4e5e-a6f8-2b0f0a307544"
+    userName="Tomate"
+    userSecret="super123"
+    />
     )
 }
+
+// infinite scroll, logout, more customizations...
+
 export default App;
