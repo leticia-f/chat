102de379324601c20033bcb242ddba270f72aa89
JulioVianaDev <juliovianadev@gmail.com>
2021-11-24 19:24:58 -0300

    subindo no dia 24/11/2021 o código

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -20,8 +20,8 @@ const ChatFeed = (props)=>{
                     <div className="message-block">
                         {
                             isMyMessage
-                            ? <MyMessage/>
-                            : <TheirMessage/>
+                            ? <MyMessage message={message}/>
+                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
                         }
                     </div>
                     <div className="read-receipts" style={{marginRight: isMyMessage ? '18px': '0px',
@@ -32,9 +32,21 @@ const ChatFeed = (props)=>{
             );
         })
     }
+    if(!chat) return 'Loading ...';
+
     return(
-        <div>
-            ChatFeed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title"> {chat.title} </div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.person.username}`)}
+                    {renderMessages()}
+                    <div style={{height: '100px'}}/>
+                    <div className="message-form-container">
+                        <MessageForm {...props} chatId={activeChat}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
